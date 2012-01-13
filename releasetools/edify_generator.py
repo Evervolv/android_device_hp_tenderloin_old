b673a3e7b603e690f66c8f4a32f7cc5dd2ba5ebd
Wong Yong Jie <yjwong92@gmail.com>
2012-01-14 04:12:27 +0800

    Properly update the uboot image
    It was disabled previously because /boot would get killed.

--- releasetools/edify_generator.py
+++ releasetools/edify_generator.py
@@ -153,6 +153,11 @@ class EdifyGenerator(object):
     destination directory."""
     self.script.append('package_extract_dir("%s", "%s");' % (src, dst))
 
+  def UnpackPackageFile(self, src, dst):
+    """Unpack a given file from the OTA package into the given
+    destination file."""
+    self.script.append('package_extract_file("%s", "%s");', % (src, dst))
+
   def Comment(self, comment):
     """Write a comment into the update script."""
     self.script.append("")

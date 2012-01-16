215c0133770a1aea33997eed88fc1a9030183cb3
andrew.boren <andrew.boren@gmail.com>
2012-01-15 22:43:22 -0700

    Tenderloin: Update props, add device specific version.
    Change-Id: Ib3e22f68cc295a7315aae50a96de233371f85002

--- ev.mk
+++ ev.mk
@@ -16,6 +16,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=tenderloin BUILD_ID=IMK74 BUILD_FIN
 
 # Set up the product codename, build version & MOTD.
 PRODUCT_CODENAME := Queo
+PRODUCT_VERSION_DEVICE_SPECIFIC := p1
 
 PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your HP Touchpad\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
 
